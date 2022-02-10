6b9bfa7110df4ace870da71247152c1c496924a0
ictechgy <ictechgy@gmail.com>
2022-02-10 20:34:49 +0900

    ✨ [Feat]: 사용자 위치권한 획득 후 화면에 나타나도록 구현

--- GreenBox/MapViewController.swift
+++ GreenBox/MapViewController.swift
@@ -8,14 +8,16 @@
 import UIKit
 import NMapsMap
 
-class MapViewController: UIViewController {
+final class MapViewController: UIViewController {
     //MARK: Properties
     @IBOutlet private weak var naverMapView: NMFNaverMapView!
+    private let locationManager = CLLocationManager()
 
     //MARK: Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setUpMapView()
+        locationManager.delegate = self
     }
 }
 
@@ -26,6 +28,38 @@ extension MapViewController {
         naverMapView.showScaleBar = true
         naverMapView.showZoomControls = true
         naverMapView.showIndoorLevelPicker = false
+    }
+}
+
+//MARK: - Conform to CLLocationManagerDelegate
+extension MapViewController: CLLocationManagerDelegate {
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        stopUsingLocationInformation()
+        
+        switch manager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .authorizedAlways, .authorizedWhenInUse:
+            prepareToUseLocationInformation()
+        case .denied, .restricted:
+            break
+        @unknown default:
+            break
+        }
+    }
+    
+    private func prepareToUseLocationInformation() {
         naverMapView.showLocationButton = true
+        
+        let fiveMeters = 5.0
+        locationManager.desiredAccuracy = kCLLocationAccuracyNearestTenMeters
+        locationManager.distanceFilter = fiveMeters
+        locationManager.startUpdatingLocation()
+    }
+
+    private func stopUsingLocationInformation() {
+        naverMapView.showLocationButton = false
+        
+        locationManager.stopUpdatingLocation()
     }
 }
